b43b4862c0bd69000694a84544de665cccacdc04
AKINDU11qwe12 <akiboydj1234@gmail.com>
2024-11-29 13:57:08 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ANJU-MD=vN0w3CBC#ToE_6P0y8eksg9ITHpReFXp6LCOGFPlzedPPcz0Nb3I",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=yNEnkTRA#ZCQHnNUWptXJEFmlJGt5e479H5upjFD-lp7OYQzjP4o",
 };
